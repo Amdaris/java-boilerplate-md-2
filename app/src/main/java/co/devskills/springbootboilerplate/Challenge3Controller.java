2671b54a35c182f4f20bed012cd64384969885fb
Oleg Rotar <oleg.rotar@amdaris.com>
2025-03-04 21:17:05 +0200

    Fix http method

--- app/src/main/java/co/devskills/springbootboilerplate/Challenge3Controller.java
+++ app/src/main/java/co/devskills/springbootboilerplate/Challenge3Controller.java
@@ -1,13 +1,13 @@
 package co.devskills.springbootboilerplate;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Challenge3Controller {
 
-    @PostMapping("/challenge-3")
+    @GetMapping("/challenge-3")
     public int implementation(
         @RequestParam("start") int start,
         @RequestParam("end") int end
